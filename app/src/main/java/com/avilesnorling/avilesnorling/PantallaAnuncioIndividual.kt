9dd3bc29a8f11a8d19b97e06d9b6df04a2cbf314
Alex <92014559+Alexgar98@users.noreply.github.com>
2023-03-27 18:55:26 +0200

    Variables anuncio individual
    Preparadas las variables de los anuncios individuales para su uso en un futuro

--- app/src/main/java/com/avilesnorling/avilesnorling/PantallaAnuncioIndividual.kt
+++ app/src/main/java/com/avilesnorling/avilesnorling/PantallaAnuncioIndividual.kt
@@ -2,10 +2,24 @@ package com.avilesnorling.avilesnorling
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class PantallaAnuncioIndividual : AppCompatActivity() {
+    var imagenPrincipal : ImageView = findViewById<ImageView>(R.id.imagenAnuncio)
+    var categoriaUbicacion : TextView = findViewById<TextView>(R.id.categoriaUbicacion)
+    var referencia : TextView = findViewById<TextView>(R.id.referenciaInteriorAnuncio)
+    var descripcion : TextView = findViewById<TextView>(R.id.txtDescripcion)
+    val btnReserva : Button by lazy {findViewById<Button>(R.id.btnReserva)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_anuncio_individual)
+
+        //TODO obtener las cosas de base de datos
+
+        btnReserva.setOnClickListener {
+            //TODO Averiguar cómo hace la conversión Avaibook para entrar a la reserva por ahí. Es inviable hacerlo con cada anuncio
+        }
     }
 }
